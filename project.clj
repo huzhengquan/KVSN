00d0a7c2c6637a6d6320d01bd10a60875a0c3b5c
胡正全 <huzhengquan@baidu.com>
2014-06-25 20:26:18 +0800

    first

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :url "http://github.com/huzhengquan/clj-kvsn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]]
-  :main clj-kvsn/foo
+  :dependencies [[org.clojure/clojure "1.6.0"]]
 )
